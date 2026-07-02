3d5cb3db88d4fc8d61d002104d38c58c3c1e915e
iakovenko_k_s <yakovkirill643@gmail.com>
2026-07-03 02:23:32 +0300

    Add more tests for SolvationTask

--- tests/solvation_task_test.rs
+++ tests/solvation_task_test.rs
@@ -13,3 +13,25 @@ fn valid_when_start_and_finish_inside_maze() {
         "task should be valid when start and finish are inside maze"
     );
 }
+
+#[test]
+fn invalid_when_start_outside_maze() {
+    let maze = Maze::new(MazeSize::new(2, 2));
+    let task = SolvationTask::new(maze, Position::new(2, 0), Position::new(1, 1));
+
+    assert!(
+        !task.valid(),
+        "task should be invalid when start is outside maze"
+    );
+}
+
+#[test]
+fn invalid_when_finish_outside_maze() {
+    let maze = Maze::new(MazeSize::new(2, 2));
+    let task = SolvationTask::new(maze, Position::new(0, 0), Position::new(0, 2));
+
+    assert!(
+        !task.valid(),
+        "task should be invalid when finish is outside maze"
+    );
+}
