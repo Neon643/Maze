0836d600e6836af500ad67f628894e57f784bf55
iakovenko_k_s <yakovkirill643@gmail.com>
2026-07-03 04:12:03 +0300

    Add available_moves, third step of dfs pipeline

--- src/solvation/dfs/mod.rs
+++ src/solvation/dfs/mod.rs
@@ -1,5 +1,6 @@
 //! Depth-first maze solvation pipeline.
 
+pub mod available_moves;
 pub mod empty_search;
 pub mod search_context;
 pub mod search_events;
@@ -10,6 +11,7 @@ pub mod search_trace;
 pub mod valid_search;
 pub mod visited_positions;
 
+pub use available_moves::AvailableMoves;
 pub use empty_search::EmptySearch;
 pub use search_context::SearchContext;
 pub use search_events::SearchEvents;
