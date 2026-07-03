2ac305abd70b9684041f795589d070c659e68adf
iakovenko_k_s <yakovkirill643@gmail.com>
2026-07-03 03:59:26 +0300

    add search state

--- src/solvation/dfs/mod.rs
+++ src/solvation/dfs/mod.rs
@@ -5,6 +5,7 @@ pub mod search_context;
 pub mod search_events;
 pub mod search_path;
 pub mod search_stage;
+pub mod search_state;
 pub mod search_trace;
 pub mod visited_positions;
 
@@ -13,5 +14,6 @@ pub use search_context::SearchContext;
 pub use search_events::SearchEvents;
 pub use search_path::SearchPath;
 pub use search_stage::SearchStage;
+pub use search_state::SearchState;
 pub use search_trace::SearchTrace;
 pub use visited_positions::VisitedPositions;
